947b3d3185e87c0cd0bdd2208f0c72c122aab6f9
ancagi <ancagiraldo@gmail.com>
2020-12-15 21:51:27 -0500

    Primera_version

---
+++ cajero_app/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <div>
+      <b-navbar toggleable="lg" type="dark" variant="info">
+        <b-navbar-brand href="#">
+          <img src="./assets/hotel.png" class="d-inline-block align-top" alt="hotelify" />
+          Hotelify
+        </b-navbar-brand>
+        <b-navbar-nav>
+          <b-nav-item href="#">Link</b-nav-item>
+        </b-navbar-nav>
+        <b-nav-item-dropdown right>
+          <!-- Using 'button-content' slot -->
+          <template #button-content>
+            <em>User</em>
+          </template>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar>
+    </div>
+    <div class="main_component"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
